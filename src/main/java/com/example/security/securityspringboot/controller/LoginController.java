56a150cb10f2b1f4edba342587e20e89ae65b419
chenliang <1484157619@qq.com>
2019-12-10 10:23:18 +0800

    add Swagger 接口Api-----http://localhost:8080/security-springboot/swagger-ui.html

--- src/main/java/com/example/security/securityspringboot/controller/LoginController.java
+++ src/main/java/com/example/security/securityspringboot/controller/LoginController.java
@@ -1,17 +1,21 @@
 package com.example.security.securityspringboot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "LoginController|一个用来测试swagger注解的控制器")//swagger在类上注解
 public class LoginController {
-
-    @RequestMapping( value = "/login-success",produces = {"text/plain;charset=UTF-8"})
+    @PostMapping( value = "/login-success",produces = {"text/plain;charset=UTF-8"})//如果用swagger动态生成文档API,如果使用RequestMapping要指定请求方法，不然swagger会出现所有请求方法(该方法get/post/delete/put..请求测试都会出现)的test API
+    @ApiOperation(value="测试登录成功与否的方法", notes="test: 简单测试")//swagger在方法上注解,RequestMapping
+//    @ApiImplicitParam(paramType="query", name = "userNumber", value = "用户编号", required = true, dataType = "Integer")//swagger对方法参数注解
     public String loginSuccess(){
 
         //提示具体用户名称登录成功
@@ -24,6 +28,7 @@ public class LoginController {
      */
     @GetMapping(value = "/r/r1",produces = {"text/plain;charset=UTF-8"})
     @PreAuthorize("hasAuthority('p1')")//拥有p1权限才可以访问
+    @ApiOperation(value="访问/r/r1", notes="测试有没有访问资源1的权限")
     public String r1(){
         return getUsername()+" 访问资源1";
     }
@@ -34,6 +39,7 @@ public class LoginController {
      */
     @GetMapping(value = "/r/r2",produces = {"text/plain;charset=UTF-8"})
     @PreAuthorize("hasAuthority('p2')")//拥有p2权限才可以访问
+    @ApiOperation(value="访问/r/r2", notes="测试有没有访问资源2的权限")
     public String r2(){
         return getUsername()+" 访问资源2";
     }
